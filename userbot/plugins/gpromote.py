10abd0e9b8e8b0aade5daf5af43b226c36dd7e0a
Deepjit Das <87997952+deepaiims@users.noreply.github.com>
2021-10-12 12:57:07 +0530

    Update gpromote.py

--- userbot/plugins/gpromote.py
+++ userbot/plugins/gpromote.py
@@ -1,4 +1,4 @@
-#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam#made by @danish_00 with sh1vam
+#made by deepaiims
 marculs=9
 from telethon.errors.rpcerrorlist import (UserIdInvalidError,
                                             MessageTooLongError)
@@ -13,7 +13,8 @@ from telethon.tl.types import (ChannelParticipantsAdmins,
                                        MessageMediaPhoto)
 from userbot.utils import register
 from userbot.utils import admin_cmd
-from userbot import bot as borg
+from userbot import catub 
+from ..core.managers import edit_del, edit_or_reply
 async def get_full_user(event):  
     args = event.pattern_match.group(1).split(':', 1)
     extra = None
@@ -40,7 +41,7 @@ async def get_full_user(event):
         try:
             user_obj = await event.client.get_entity(user)
         except Exception as err:
-            return await event.edit("Error... Please report at @Deviluserbot", str(err))           
+            return await event.edit("Error... Please report at @catuserbot", str(err))           
     return user_obj, extra
 
 global hawk,moth
@@ -55,13 +56,24 @@ async def get_user_from_id(user, event):
         await event.edit(str(err))
         return None
     return user_obj
-@borg.on(admin_cmd(pattern="gpromote ?(.*)"))
+  
+plugin_category="extra"
+
+@catub.cat_cmd(
+    pattern="gpromote(?:\s|$)([\s\S]*)", 
+    command=("gpromote", plugin_category), 
+    info={
+        "header": "gpromote.",   
+        "description": "use for more admin rights",
+        "usage": "{tr}hibuddy <text>",
+    }
+)
 async def gben(userbot):
-    dc = dark = userbot
+    dc = cat = userbot
     i = 0
     sender = await dc.get_sender()
     me = await userbot.client.get_me()
-    await dark.edit("`promoting...`")
+    await cat.edit("`promoting...`")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
     f"@{me.username}" if me.username else my_mention
     await userbot.get_chat()
@@ -75,13 +87,13 @@ async def gben(userbot):
     except:
         pass
     if me == user:
-       k = await dark.edit("U want to promote urself 😑😑 waao..")
+       k = await cat.edit("U want to promote urself 😑😑 waao..")
        return
     try:
         if not rank:
             rank = "ㅤㅤ"
     except:
-        return await dark.edit(f"**Something W3NT Wrong 🤔**")
+        return await cat.edit(f"**Something W3NT Wrong 🤔**")
     if user:
         telchanel = [d.entity.id
                      for d in await userbot.client.get_dialogs()
@@ -97,21 +109,29 @@ async def gben(userbot):
           try:
              await userbot.client(EditAdminRequest(x, user, rgt, rank))
              i += 1
-             await dark.edit(f"**Promoted in Chats **: `{i}`")
+             await cat.edit(f"**Promoted in Chats **: `{i}`")
           except:
              pass
     else:
-        await dark.edit(f"**Reply to a user you dumbo !!**")
-    return await dark.edit(
+        await cat.edit(f"**Reply to a user you dumbo !!**")
+    return await cat.edit(
         f"**Globally promoted [{user.first_name}](tg://user?id={user.id})\n On Chats😏 : {i} **"
-    )
-@borg.on(admin_cmd(pattern="gdemote ?(.*)"))
+    
+@catub.cat_cmd(
+    pattern="gdemote ?(.*)",
+    command=("gdemote", plugin_category), 
+    info={
+        "header": "To gdemote",
+        "usage": "{tr}gdemote",
+        "examples": "{tr}gdemote username",
+    }
+) 
 async def gben(userbot):
-    dc = dark = userbot
+    dc = cat = userbot
     i = 0
     sender = await dc.get_sender()
     me = await userbot.client.get_me()
-    await dark.edit("`demoting...`")
+    await cat.edit("`demoting...`")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
     f"@{me.username}" if me.username else my_mention
     await userbot.get_chat()
@@ -125,13 +145,13 @@ async def gben(userbot):
     except:
         pass
     if me == user:
-       k = await dark.edit("U want to demote urself 😑😑 waao..")
+       k = await cat.edit("U want to demote urself 😑😑 waao..")
        return
     try:
         if not rank:
             rank = "ㅤㅤ"
     except:
-        return await dark.edit(f"**Something W3NT Wrong 🤔**")
+        return await cat.edit(f"**Something W3NT Wrong 🤔**")
     if user:
         telchanel = [d.entity.id
                      for d in await userbot.client.get_dialogs()
@@ -147,12 +167,12 @@ async def gben(userbot):
           try:
              await userbot.client(EditAdminRequest(x, user, rgt, rank))
              i += 1
-             await dark.edit(f"**Demoted in Chats **: `{i}`")
+             await cat.edit(f"**Demoted in Chats **: `{i}`")
           except:
              pass
     else:
-        await dark.edit(f"**Reply to a user you dumbo !!**")
-    return await dark.edit(
+        await cat.edit(f"**Reply to a user you dumbo !!**")
+    return await cat.edit(
         f"**Globally Demoted [{user.first_name}](tg://user?id={user.id})\n On Chats😏 : {i} **"
     )
 
